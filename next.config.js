9da71c4ebaa1ff8ad2ee332f4a8bce2e98b1ab86
Santirbe98 <santirbe@gmail.com>
2023-08-05 16:29:29 -0300

    nextConfigChanges

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const fs = require("fs");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-const env = dotenv.parse(fs.readFileSync(".env"));
+// const env = dotenv.parse(fs.readFileSync(".env"));
 
 module.exports = {
   reactStrictMode: true,
-  env: env,
+  // env: env,
   sassOptions: {
     includePaths: [path.join(__dirname, "css")],
   },
