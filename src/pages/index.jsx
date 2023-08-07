7148106146fc7306656d2bf0f2b774fb3689d60b
Santirbe98 <santirbe@gmail.com>
2023-08-07 15:51:43 -0300

    fix(carrito)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,6 @@ import Homepage1 from "./homepage/home6-light";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
 const Home = () => {
-  const [cart, setCart] = useLocalStorage("cart", []);
-
-  useEffect(() => {
-    // evita errores al cargar por primera vez
-
-    setCart([]);
-  }, [setCart]);
   return (
     <>
       <Homepage1 />
