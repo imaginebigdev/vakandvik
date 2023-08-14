7d03ff5163c03cba7437dc8aecdbbb01ce778fff
gaspar <gasparmunoz@hotmail.com>
2023-08-13 21:13:42 -0300

    modifique navbar en admin panel

--- src/components/admin/index.jsx
+++ src/components/admin/index.jsx
@@ -8,53 +8,72 @@ import NavbarAdmin from "../Navbar/navbar-admin";
 import FormAdmin from "../Form-admin/form-admin";
 import CategoriesAdmin from "../Categories-component/categories-component";
 import FormAdminCategories from "../Categories-component/form-admin-categories";
-import Intro6 from "../Intro6/intro6";
 import IntegrationEsp from "../Stack-admin/Stack-admin";
-import Particles, { Particle } from "react-tsparticles";
 
 const Admin = () => {
   const [show, setShow] = useState("orders");
   const [modal, setModal] = useState(false);
 
+  const MainContent = React.useRef(null);
+  const navbarRef = React.useRef(null);
+  const logoRef = React.useRef(null);
   /*   const abrirModal = (e) => {
     e.preventDefault();
     setModal(true);
   }; */
 
+  React.useEffect(() => {
+    var navbar = navbarRef.current;
+    if (window.pageYOffset > 300) {
+      navbar.classList.add("nav-scroll");
+    } else {
+      navbar.classList.remove("nav-scroll");
+    }
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        navbar.classList.add("nav-scroll");
+      } else {
+        navbar.classList.remove("nav-scroll");
+      }
+    });
+  }, [MainContent, navbarRef]);
+
   return (
     <LightTheme mobileappstyle>
-      <NavbarAdmin theme="themeL" />
-      <div className="welcome-section">
-        <h1 className="title-admin pt-80">Bienvenid@ Admin</h1>
-      </div>
-      <section className="shop section-padding ">
-        <div className="container pb-80">
-          <div className="row">
-            <div className="col-lg-3">
-              <AdminSidebar setShow={setShow} show={show} />
-            </div>
-            <div className="col-lg-9">
-              {show === "orders" && <OrdersComponent />}
-              {show === "products" && (
-                <>
-                  <ProductsAdmin />
+      <NavbarAdmin nr={navbarRef} lr={logoRef} theme="themeL" />
+      <div ref={MainContent} className="main-content">
+        <div className="welcome-section">
+          <h1 className="title-admin pt-80">Bienvenid@ Admin</h1>
+        </div>
+        <section className="shop section-padding">
+          <div className="container pb-80">
+            <div className="row">
+              <div className="col-lg-3">
+                <AdminSidebar setShow={setShow} show={show} />
+              </div>
+              <div className="col-lg-9">
+                {show === "orders" && <OrdersComponent />}
+                {show === "products" && (
+                  <>
+                    <ProductsAdmin />
 
-                  <FormAdmin setModal={setModal} modal={modal} />
-                </>
-              )}
-              {show === "categories" && (
-                <>
-                  <CategoriesAdmin />
+                    <FormAdmin setModal={setModal} modal={modal} />
+                  </>
+                )}
+                {show === "categories" && (
+                  <>
+                    <CategoriesAdmin />
 
-                  <FormAdminCategories setModal={setModal} modal={modal} />
-                </>
-              )}
+                    <FormAdminCategories setModal={setModal} modal={modal} />
+                  </>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <IntegrationEsp />
-      <Footer2 />
+        </section>
+        <IntegrationEsp />
+        <Footer2 />
+      </div>
     </LightTheme>
   );
 };
